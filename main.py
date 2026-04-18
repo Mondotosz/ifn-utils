bc94265fa6b70bef9acf341af9f76634cfb85f40
Mondotosz <git@moncorp.ch>
2026-04-18 11:24:35 +0200

    feat: added timezone and daylight saving commands

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import typer
 from ifn.mbr import mbr_app
 from ifn.gpt import gpt_app
+from ifn.timezone import tz_app
 
 app = typer.Typer(help="MBR Partition Table Analyzer")
 
 app.add_typer(mbr_app, name="mbr")
 app.add_typer(gpt_app, name="gpt")
+app.add_typer(tz_app, name="tz")
 
 
 if __name__ == "__main__":
